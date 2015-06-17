242008db0bb6de33ba9ccba4132ff236953b1e91
wilstond <mail2wilston@gmail.com>
2015-06-16 20:12:16 -0400

    Publish / Delete feature Complete

---
+++ app/art.php
@@ -0,0 +1,17 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Description of art
+ *
+ * @author Wilston
+ */
+class art extends Model {
+    
+    protected $table = "art";
+    
+    public $timestamps = false;
+    
+}
