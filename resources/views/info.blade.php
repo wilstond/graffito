5d62820e6b98b261833257c32d9f4675ff9016ef
craigprofessional <craigveenstra@gmail.com>
2015-06-22 15:28:23 -0400

    Info Page

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -8,13 +8,11 @@
 <div id="bg_image"></div>
     <div id="blackoverlay" ></div>
        
-           
+    <div id="info" >
             <h1>Information</h1>
 
-            <p>Graffito allows users to document and share details, images and locations of 
+            <p>Graffito documents and shares details, images and locations of 
                 street art around the city of Toronto.</p>
-
-            <br />
 
             <!--            <div>The main objective of Graffito is to provide walking tours of street art around 
                         the city while also providing the dual function of acting as a catalogue which 
@@ -23,15 +21,15 @@
                         media sites and contribute new entries to the application as new works are 
                         physically created around the city. </div>  -->
 
-            <p>Graffito will take the museum experience outdoors, creating a celebration of 
-                street art, the artists and past works in the city of Toronto. </p>
-            <br />
+            <p>Take the museum experience outdoors, celebrate 
+                street art, the artists and remember past works in the city of Toronto. </p>
+            
             <p>Explore existing entries, Contribute new ones, and Explore Toronto. </p>
 
             <br />
 
             <p>Contact the developers -> </p>
-      
+      </div>
         </header>
 
 
