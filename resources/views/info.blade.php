6e93240cc9b6e139fa483202e48efdf6f8e95a6e
Brittany Trafford <traffordbrittany@gmail.com>
2015-06-22 15:33:19 -0400

    info

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -14,27 +14,16 @@
             <p>Graffito documents and shares details, images and locations of 
                 street art around the city of Toronto.</p>
 
-            <!--            <div>The main objective of Graffito is to provide walking tours of street art around 
-                        the city while also providing the dual function of acting as a catalogue which 
-                        archives past works by location, giving users a way to look back in time. 
-                        Graffito also provides the ability for users to share posts on other social 
-                        media sites and contribute new entries to the application as new works are 
-                        physically created around the city. </div>  -->
-
             <p>Take the museum experience outdoors, celebrate 
-                street art, the artists and remember past works in the city of Toronto. </p>
+                street art, the artists and remember past works in the city. </p>
             
             <p>Explore existing entries, Contribute new ones, and Explore Toronto. </p>
 
             <br />
 
-            <p>Contact the developers -> </p>
+            
       </div>
         </header>
-
-
-
-
 @endsection
 
 
