b861ccd8f327315404ffd2536fbc5b540de0b1c5
wilstond <mail2wilston@gmail.com>
2015-06-23 14:57:48 -0400

    Explore module begun
    Wrote the query to fetch the coordinates from the database

---
+++ app/location.php
@@ -0,0 +1,17 @@
+<?php
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Description of location
+ *
+ * @author Wilston
+ */
+class location extends Model {
+    
+    protected $table = "location";
+    
+    public $timestamps = false;
+    
+}
